8aa60a87049919810c605955c3eb6a69a12944f1
Shybert <25749775+Shybert@users.noreply.github.com>
2019-01-23 12:35:14 +0100

    Added color mults

--- index.js
+++ index.js
@@ -9,6 +9,28 @@ registerPatcher({
     defaultSettings: {}
   },
   execute: (patchFile, helpers, settings, locals) => ({
+    initialize: (patchFile, helpers, settings, locals) => {
+      const base = -0.94
+      locals.mults = {
+        nightMult: 1 + base,
+        duskDawnMult: 1 + base * 0.5,
+        ambientNightMult: 1 + base * 0.825,
+        ambientDuskDawnMult: 1 + base * 0.4,
+        ambientDayMult: 1 + base,
+        effectNightMult: 1 + base * 0.625,
+        effectDuskDawnMult: 1 + base * 0.5,
+        effectDayMult: 1 + base * 0.35,
+        mystNightMult: 1 + base * 0.75,
+        mystDuskDawnMult: 1 + base * 0.85,
+        rainNightMult: 1 + base * 0.95,
+        rainDuskDawnMult: 1 + base * 0.2,
+        magicNightMult: 1 + (base * 0.5) - 0.3,
+        magicDuskDawnMult: 1 + (base * 0.6) - 0.1,
+        skyNightMult: 1 + base * 0.625,
+        skyDuskDawnMult: 1 + base * 0.25,
+        starsMult: 0.8
+      }
+    },
     process: [{
       load: {
         signature: 'WTHR'
